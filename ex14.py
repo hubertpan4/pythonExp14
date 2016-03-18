f52b528b3ae1b241713c48e6c3389f04f19e1447
Hubert Pan <hubert@Huberts-Mac-mini.local>
2016-03-18 01:31:19 -0400

    initializing repo

---
+++ ex14.py
@@ -0,0 +1,58 @@
+from sys import argv
+import random
+import logging
+
+logging.basicConfig(level = logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+prompt = '> '
+
+try:
+    script = argv[0]
+    userName = argv[1]
+except IndexError:
+    userName = 'default'
+
+
+def init_conversation():
+    """ Handles the conversation """
+    print("Hi ", userName, ", I am the ", script, " script.")
+
+    hasNumQuestions = False
+    numQuestions = -1
+
+    while hasNumQuestions == False:
+        print("How many questions would you like to answer?")
+        numQuestionsStr = input(prompt)
+
+        try:
+            numQuestions = int(numQuestionsStr)
+            hasNumQuestions = True
+        except:
+            print("Unable to parse \'" + numQuestionsStr +
+            "\' as an integer, please enter an integer")
+            pass
+    print("Preparing " + str(numQuestions) + " questions.")
+
+    for i in range(0, numQuestions):
+        questionStr = str(i) + ' + ' + str(random.randint(0, numQuestions))
+        print('Question ' + str(i + 1) + ': ' + questionStr)
+        ans = input(prompt)
+
+        # if type(ans) == int:
+        #     ans = int(ans)
+        #     logging.warn('ans convert into an int')
+
+        expected = eval(questionStr)
+
+        print(type(ans))
+        print(type(expected))
+
+        if ans == str(expected):
+            print('Correct!')
+        else:
+            print("Incorrect, the correct answer is " + str(expected) + " not "
+            + str(ans) + "!")
+
+if __name__ == '__main__':
+    init_conversation()
